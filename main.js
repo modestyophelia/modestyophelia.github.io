8a0805cbefe274239921baaaba48da75d5b2bb4c
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2021-09-16 20:51:44 +0200

    website base design
    base design for the website, to be further build on

---
+++ main.js
@@ -0,0 +1,33 @@
+
+function myFunction(){
+
+    console.log("Hello from the console! *waves*");
+}
+
+myFunction();
+
+console.log("hmmm");
+
+document.getElementById("paragraph").innerHTML = "<p>I overwrote you</p>"
+document.getElementsByName("")
+
+for(var i = 0; i<10; i++){
+
+    console.log(i+1);
+}
+
+if (1+1 == 5){
+
+    console.log("is 1+1 really 5 ?");
+}
+else {
+    console.log("whats 1+1 then ? ");
+}
+
+try {
+var wrongID = document.getElementsById("im wrong");
+wrongID.innerHTML = "<p>im sooo wrong</p>"
+
+} catch(error) {
+    console.error("Apparently something went wrong: " + error)
+}
