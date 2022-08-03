ec05ac176f3c3ecc5234a4b8feeb7ca140c7a7f9
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2022-08-03 17:50:31 +0200

    update

--- main.js
+++ main.js
@@ -1,46 +1,28 @@
-
-
 var counter = 0;
 
-function handleClick(){
+function handleClick() {
+  counter += 1;
 
-    counter+= 1;
+  document.getElementById("counter").innerHTML = counter;
 
-    document.getElementById("counter").innerHTML = counter;
-
-    if (counter < 2){
-        alert("You booped the snoot. Fox thanks you!");
-    }
-
-    else if (counter < 3) {
-        alert("You booped the snoot. Fox really likes you!");
-    }
-
-    else {
-        alert("You booped the snoot. Fox loves you!");
-        document.getElementById("heart").style.visibility = "visible";
-        
-    } 
+  if (counter < 2) {
+    alert("You booped the snoot. Fox thanks you!");
+  } else if (counter < 3) {
+    alert("You booped the snoot. Fox really likes you!");
+  } else {
+    alert("You booped the snoot. Fox loves you!");
+    document.getElementById("heart").style.visibility = "visible";
+  }
 }
 
-btn.addEventListener("click", e => {
-    console.log("You clicked the fox")
+btn.addEventListener("click", (e) => {
+  console.log("You clicked the fox");
 });
 
-heart.addEventListener("mouseover", function(){
+heart.addEventListener("mouseover", function () {
+  console.log("Your mouse went over the heart");
+});
 
-    console.log("Your mouse went over the heart")
-})
-
-for(var i = 0; i<10; i++){ 
-
-    console.log(i+1);
-}
-
-try { 
-var wrongID = document.getElementsById("im wrong");
-wrongID.innerHTML = "<p>im sooo wrong</p>"
-
-} catch(error) {
-    console.error("Apparently something went wrong: " + error)
+for (var i = 0; i < 10; i++) {
+  console.log(i + 1);
 }
